3065cbe7cff03943665964c95b581446fa74a586
Ammar Ogeil <95254224+Goldenhawk2003@users.noreply.github.com>
2024-11-07 11:26:21 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import BecomeAContractor from './components/BecomeAContractor';
-import Contact from './components/Contact.js';
+import Login from './components/Auth/Login';
+import Signup from './components/Auth/Signup'; // Added Signup import
+import ClientProfile from './components/Clients/ ClientProfile';
+import ClientForm from './components/Clients/ClientForm';
+import ClientsList from './components/Clients/ClientsList';
+import Contact from './components/Contact';
+import ContractorForm from './components/Contractors/ContractorForm';
+import ContractorProfile from './components/Contractors/ContractorProfile';
+import ContractorsList from './components/Contractors/ContractrorsList.js';
+import Dashboard from './components/Dashboard/Dashboard'; // Added Dashboard import
 import Home from './components/Home';
-import Jobs from './components/Jobs';
-import Layout from './components/Layout.js';
-import Login from './components/Login';
+import Layout from './components/Layout';
+
+
 
 function App() {
   return (
@@ -14,10 +22,20 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="become-a-contractor" element={<BecomeAContractor />} />
-          <Route path="jobs" element={<Jobs />} />
-          <Route path="login" element={<Login />} />
-          <Route path="contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          {/* Client-related routes */}
+          <Route path="clients" element={<ClientsList />} />
+          <Route path="clients/:id" element={<ClientProfile />} />
+          <Route path="clients/edit/:id" element={<ClientForm />} />
+          <Route path="clients/add" element={<ClientForm />} />
+          {/* Contractor-related routes */}
+          <Route path="contractors" element={<ContractorsList />} />
+          <Route path="contractors/:id" element={<ContractorProfile />} />
+          <Route path="contractors/edit/:id" element={<ContractorForm />} />
+          <Route path="contractors/add" element={<ContractorForm />} />
         </Route>
       </Routes>
     </Router>
