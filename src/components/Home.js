37a87b8c3e37b086c35f8dec78522c2a26e03e4f
Ammar Ogeil <ammarogeil@gmail.com>
2024-11-21 16:33:10 -0500

    adjusted home page UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,22 +18,39 @@ const Home = () => {
       <h1 className="head">Welcome To Elite Crafts Contractors</h1>
       <p className="subhead">Your trusted platform for skilled contractors in every field.</p>
       
-      <div className="search-bar-container">
-        <input
-          type="text"
-          className="search-bar"
-          placeholder="Search services: Plumbing, renovations, snow removal"
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-          onKeyPress={handleKeyPress}
-        />
-        <i className="fas fa-search search-icon"></i>
-      </div>
-
+      <div class="search-bar-container">
+  <input type="text" class="search-bar" placeholder="Search services: Plumbing, renovations, snow removal" />
+  <button class="search-button">
+  <i className="fas fa-search search-icon"></i>
+  </button>
+</div>
+      
       <div className="services-buttons">
         
         <Link to="/ServiceRequest" className="service-button">Service Request</Link> 
         <Link to="/find-contractor" className='service-button'>Find a Contractor</Link>
+      </div>
+      <div className="Contractor-choices">
+        <button type='submit'>Washroooms<img src="/images/download.png" alt="Bathroom Renovation Ideas" /></button>
+        <button type='submit'>Washroooms<img src="/images/download.png" alt="Bathroom Renovation Ideas" /></button>
+        <button type='submit'>Washroooms<img src="/images/download.png" alt="Bathroom Renovation Ideas" /></button>
+        <button type='submit'>Washroooms<img src="/images/download.png" alt="Bathroom Renovation Ideas" /></button>
+        <button type='submit'>Washroooms<img src="/images/download.png" alt="Bathroom Renovation Ideas" /></button>
+        <button type='submit'>Washroooms<img src="/images/download.png" alt="Bathroom Renovation Ideas" /></button>
+      </div>
+      <div className='trust'>
+        <h2 className='trust-header'>Trustworthy People, With reliable solutions
+        </h2>
+        <p className='trust-p'>Our quiz is designed to get the stress of looking for a contractor off your plate</p>
+
+        <button type='submit' className='trust-button'>Take Quiz now</button>
+      </div>
+      <div className='our-process'> 
+        <h2 className='our-header'>Our Process</h2>
+
+      </div>
+      <div className='match'>
+        <h2 className='match-header'>Match With Our Professionals Today.</h2>
       </div>
     </div>
   );
