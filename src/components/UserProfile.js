555fc678d92a2f35e4b917bfb874093f18f82f86
Ammar Ogeil <ammarogeil@gmail.com>
2025-03-31 22:45:14 -0400

    Update UserProfile.js

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -120,9 +120,7 @@ const HomeTab = ({ userInfo }) => {
             />
           )}
           <div className="profile-info">
-            <button className="submit-btn" onClick={editProfile}>
-              Edit Profile
-            </button>
+           
           </div>
           <div className="profile-container">
             <p className="profile-username">{userInfo.username}</p>
