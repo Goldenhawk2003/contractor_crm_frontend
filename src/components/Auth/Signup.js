0521d004729798de21d6f485a0f7849781b5edc1
Ammar Ogeil <ammarogeil@gmail.com>
2025-04-08 01:14:11 -0400

    Update Signup.js

--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import LocationAutocomplete from '../APIStuff/AutoComplete';
 import './Signup.css';
 
 function Signup() {
@@ -13,7 +14,7 @@ function Signup() {
     role: 'client',
     job_type: '',
     hourly_rate: '',
-    logo: null,
+    logo: null, // New field for logo
   });
 
   const [error, setError] = useState('');
@@ -48,14 +49,28 @@ function Signup() {
     const { username, email, password, confirmPassword, hourly_rate, role, logo } = formData;
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (!username) return "Username is required";
-    if (!email || !emailPattern.test(email)) return "Please enter a valid email address";
-    if (password.length < 6) return "Password should be at least 6 characters";
-    if (password !== confirmPassword) return "Passwords do not match";
+    if (!username) {
+      return "Username is required";
+    }
+    if (!email || !emailPattern.test(email)) {
+      return "Please enter a valid email address";
+    }
+    if (password.length < 6) {
+      return "Password should be at least 6 characters";
+    }
+    if (password !== confirmPassword) {
+      return "Passwords do not match";
+    }
     if (role === 'professional') {
-      if (!formData.job_type) return "Job type is required for professionals";
-      if (!hourly_rate || isNaN(hourly_rate) || hourly_rate <= 0) return "Hourly rate must be a positive number";
-      if (!logo) return "Logo is required for professionals";
+      if (!formData.job_type) {
+        return "Job type is required for professionals";
+      }
+      if (!hourly_rate || isNaN(hourly_rate) || hourly_rate <= 0) {
+        return "Hourly rate must be a positive number";
+      }
+      if (!logo) {
+        return "Logo is required for professionals";
+      }
     }
     return null;
   };
@@ -108,67 +123,67 @@ function Signup() {
 
   return (
     <div className='signup-page'>
-      <h1 className='sign-up-header'>Welcome to ETN</h1>
-      <form onSubmit={handleSubmit} className="cont">
+    <h1 className='sign-up-header'>Welcome to ETN</h1>
+    <form onSubmit={handleSubmit} className="cont">
 
-        {error && <p style={{ color: 'red' }}>{error}</p>}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      
+      <input
+        type="text"
+        name="username"
+        value={formData.username}
+        onChange={handleChange}
+        placeholder="Username"
+        required
+        className='inp'
+      />
+      <input
+        type="text"
+        name="firstname"
+        value={formData.firstname}
+        onChange={handleChange}
+        placeholder="First Name"
+        required
+        className='inp'
+      />
+      <input
+        type="text"
+        name="lastname"
+        value={formData.lastname}
+        onChange={handleChange}
+        placeholder="Last Name"
+        required
+        className='inp'
+      />
+      <input
+        type="email"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+        placeholder="Email"
+        required
+        className='inp'
+      />
+      <input
+        type="password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+        placeholder="Password"
+        required
+        className='inp'
+      />
+      <input
+        type="password"
+        name="confirmPassword"
+        value={formData.confirmPassword}
+        onChange={handleChange}
+        placeholder="Confirm Password"
+        required
+        className='inp'
+      />
 
-        <input
-          type="text"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-          placeholder="Username"
-          required
-          className='inp'
-        />
-        <input
-          type="text"
-          name="firstname"
-          value={formData.firstname}
-          onChange={handleChange}
-          placeholder="First Name"
-          required
-          className='inp'
-        />
-        <input
-          type="text"
-          name="lastname"
-          value={formData.lastname}
-          onChange={handleChange}
-          placeholder="Last Name"
-          required
-          className='inp'
-        />
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          placeholder="Email"
-          required
-          className='inp'
-        />
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          placeholder="Password"
-          required
-          className='inp'
-        />
-        <input
-          type="password"
-          name="confirmPassword"
-          value={formData.confirmPassword}
-          onChange={handleChange}
-          placeholder="Confirm Password"
-          required
-          className='inp'
-        />
-
-        <input
+      <input
           id="location-autocomplete"
           type="text"
           placeholder="Enter location"
@@ -177,8 +192,75 @@ function Signup() {
           onChange={handleChange}
         />
 
-        <button className='sbmit' type="submit">Sign Up</button>
-      </form>
+      <div className='role'>
+      <label>
+        <input
+          type="radio"
+          name="role"
+          value="client"
+          checked={formData.role === 'client'}
+          onChange={handleChange}
+          className='radio'
+        />
+    I am a Homeowner
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="role"
+          value="professional"
+          checked={formData.role === 'professional'}
+          onChange={handleChange}
+          className='radio'
+        />
+      I am a Contractor
+      </label>
+      </div>
+      
+      {formData.role === 'professional' && (
+        <>
+          <select
+      name="job_type"
+      value={formData.job_type}
+      onChange={handleChange}
+      required
+      className="jobtype"
+    >
+      <option value="" disabled>Select Profession</option>
+      <option value="Plumbing">Plumber</option>
+      <option value="Electrical">Electrician</option>
+      <option value="Carpentery">Carpenter</option>
+      <option value="Renovation">Renovator</option>
+      <option value="Painting">Painter</option>
+      <option value="Landscaper">Landscaper</option>
+      <option value="Mechanic">Mechanic</option>
+      <option value="Welder">Welder</option>
+      <option value="Mason">Mason</option>
+      <option value="HVAC Technician">HVAC Technician</option>
+      <option value="Snow Removal">Snow Removal</option>
+    </select>
+          <input
+            type="text"
+            name="hourly_rate"
+            value={formData.hourly_rate}
+            onChange={handleChange}
+            placeholder="Hourly Rate (e.g., 50)"
+            required
+            className='inp'
+          />
+          <input
+            type="file"
+            name="logo"
+            onChange={handleChange}
+            accept="image/*"
+            required
+            className='inp'
+          />
+        </>
+      )}
+      
+      <button className='sbmit' type="submit">Sign Up</button>
+    </form>
     </div>
   );
 }
